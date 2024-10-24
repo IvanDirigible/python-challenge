b5d556b2ba0e80e9631b52588202f93734f76764
IvanDirigible <sickeruncible@gmail.com>
2024-10-24 12:14:34 -0400

    Correctly returning winner name now. Need to get it printing properly, then I can clean it up.

--- PyPoll/main copy.py
+++ PyPoll/main copy.py
@@ -12,11 +12,14 @@ file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output fil
 # Initialize variables to track the election data
 total_votes = 0  # Track the total number of votes cast
 current_cand = ""
-
+win_calc = ""
 # Define lists and dictionaries to track candidate names and vote counts
 cand_dict = {
    "total": total_votes,
-   "candidates": {
+   "votes": {
+      
+   },
+   "percent": {
       
    }
 }
@@ -46,15 +49,15 @@ with open(file_to_load) as election_data:
       current_cand = row[2]
 
         # If the candidate is not already in the candidate list, add them
-      if current_cand not in cand_dict["candidates"].keys():
+      if current_cand not in cand_dict["votes"].keys():
          #cand_dict["Name"] = current_cand
          #cand_dict["Votes"] = 1
-         cand_dict["candidates"][current_cand] = 0
+         cand_dict["votes"][current_cand] = 0
          #cand_list.append(cand_dict)
          #print(cand_list[cand_dict[0]["Name"]])
 
         # Add a vote to the candidate's count
-      cand_dict["candidates"][current_cand] = cand_dict["candidates"][current_cand] + 1
+      cand_dict["votes"][current_cand] = cand_dict["votes"][current_cand] + 1
 
 # Open a text file to save the output
 with open(file_to_output, "w") as txt_file:
@@ -72,16 +75,22 @@ Total Votes: {cand_dict["total"]}"
     # Write the total vote count to the text file
 
     # Loop through the candidates to determine vote percentages and identify the winner
-  for i in cand_dict["candidates"].keys():
+  for i in cand_dict["votes"].keys():
      # print(f"Look here: {cand_dict}")
      #print(f"Candidates: {cand_dict["candidates"][i]}")
-     print(i)
+     #print(i)
      # cand_dict["percent"] = f"{'{:.3%}'.format(cand_dict["candidates"][i] / cand_dict["total"])}"
-     cand_dict["percent"] = {i:f"{'{:.3%}'.format(cand_dict["candidates"][i] / cand_dict["total"])}"}
+     #if cand_dict["percent"][i] not in cand_dict["percent"].keys():
+     cand_dict["percent"][i] = f"{'{:.3%}'.format(cand_dict['votes'][i] / cand_dict['total'])}"
+     print(f"Percent: {cand_dict['percent']}")
      #percent_votes = cand_dict[i] / total_votes
      #cand_dict[f"Percent{percent_votes}"] = percent_votes
      #perc_dict = {"Total": cand_dict[i], "Percent": percent_votes}
-  print(f"Final Dictionary: {cand_dict}")
+     if cand_dict["percent"][i] > win_calc:
+       win_calc = cand_dict["percent"][i]
+       winner = i
+  print(f"Final Dictionary: {cand_dict}\n\
+        Winner: {winner}")
         # Get the vote count and calculate the percentage
 
 
