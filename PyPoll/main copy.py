f0d68769b5f7ec244778ad8e2a558df40a0fe7e2
IvanDirigible <sickeruncible@gmail.com>
2024-10-24 12:33:58 -0400

    Summary text now printing to terminal perfectly.

--- PyPoll/main copy.py
+++ PyPoll/main copy.py
@@ -13,6 +13,7 @@ file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output fil
 total_votes = 0  # Track the total number of votes cast
 current_cand = ""
 win_calc = ""
+summary = ""
 # Define lists and dictionaries to track candidate names and vote counts
 cand_dict = {
    "total": total_votes,
@@ -63,12 +64,12 @@ with open(file_to_load) as election_data:
 with open(file_to_output, "w") as txt_file:
 
     # Print the total vote count (to terminal)
-  print(
-f"\n\
+# print(
+  summary = f"""\n 
 Election Results\n\
 -------------------------\n\
-Total Votes: {cand_dict["total"]}"
-  )
+Total Votes: {cand_dict["total"]}\n\
+-------------------------\n"""
 
   print(f"Dictionary: {cand_dict}")
   #print(f"List: {cand_list}")
@@ -81,26 +82,27 @@ Total Votes: {cand_dict["total"]}"
      #print(i)
      # cand_dict["percent"] = f"{'{:.3%}'.format(cand_dict["candidates"][i] / cand_dict["total"])}"
      #if cand_dict["percent"][i] not in cand_dict["percent"].keys():
-     cand_dict["percent"][i] = f"{'{:.3%}'.format(cand_dict['votes'][i] / cand_dict['total'])}"
-     print(f"Percent: {cand_dict['percent']}")
+
      #percent_votes = cand_dict[i] / total_votes
      #cand_dict[f"Percent{percent_votes}"] = percent_votes
      #perc_dict = {"Total": cand_dict[i], "Percent": percent_votes}
-     if cand_dict["percent"][i] > win_calc:
-       win_calc = cand_dict["percent"][i]
-       winner = i
-  print(f"Final Dictionary: {cand_dict}\n\
-        Winner: {winner}")
-        # Get the vote count and calculate the percentage
 
+        # Get the vote count and calculate the percentage
+    #cand_dict["percent"][i] = f"{'{:.3%}'.format(cand_dict['votes'][i] / cand_dict['total'])}"
+    cand_dict["percent"][i] = f"{'{:.3%}'.format(cand_dict['votes'][i] / cand_dict['total'])}"
+    print(f"Percent: {cand_dict['percent']}")
 
         # Update the winning candidate if this one has more votes
-
-
+    if cand_dict["percent"][i] > win_calc:
+      win_calc = cand_dict["percent"][i]
+      winner = i
+  
         # Print and save each candidate's vote count and percentage
-
+    #summary = summary + (f"{i}: {'{:.3%}'.format(cand_dict["percent"][i])} ({cand_dict['votes'][i]})")
+    summary = summary + (f"{i}: {cand_dict["percent"][i]} ({cand_dict['votes'][i]})\n")
 
     # Generate and print the winning candidate summary
-
+  summary = summary + (f"-------------------------\nWinner: {winner}\n-------------------------")
 
     # Save the winning candidate summary to the text file
+print(summary)
