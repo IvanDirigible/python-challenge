c099cf2d5d093c6538834251479c9eae51d4ce9b
IvanDirigible <sickeruncible@gmail.com>
2024-10-24 12:59:50 -0400

    Removed copy.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,3 @@
-# -*- coding: UTF-8 -*-
-"""PyPoll Homework Starter File."""
-
 # Import necessary modules
 import csv
 import os
@@ -10,17 +7,18 @@ file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
 file_to_output = os.path.join("analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables to track the election data
-total_votes = 0  # Track the total number of votes cast
+current_cand = "" # Selecting candidate for vote counting
+win_calc = "" # Comparison for winning percentage
+summary = "" # Text to write summary
 
 # Define lists and dictionaries to track candidate names and vote counts
-cand_list = []
-cand_dict ={}
-current_cand = ""
-# {"candidate":"","votes":0}
+cand_dict = {
+  "total": 0, # Tracks total votes
+  "votes": {}, # Tracks votes for each candidate
+  "percent": {} # Tracks percentage of votes
+}
 
 # Winning Candidate and Winning Count Tracker
-
-
 # Open the CSV file and process it
 with open(file_to_load) as election_data:
     reader = csv.reader(election_data)
@@ -35,55 +33,43 @@ with open(file_to_load) as election_data:
       print(". ", end="")
 
         # Increment the total vote count for each row
-      total_votes = total_votes + 1
+      cand_dict["total"] = cand_dict["total"] + 1
 
         # Get the candidate's name from the row
       current_cand = row[2]
 
         # If the candidate is not already in the candidate list, add them
-      if current_cand not in cand_dict.keys():
-         #cand_dict["Name"] = current_cand
-         #cand_dict["Votes"] = 1
-         cand_dict[current_cand] = 0
-         #cand_list.append(cand_dict)
-         #print(cand_list[cand_dict[0]["Name"]])
+      if current_cand not in cand_dict["votes"].keys():
+         cand_dict["votes"][current_cand] = 0
 
         # Add a vote to the candidate's count
-      cand_dict[current_cand] = cand_dict[current_cand] + 1
+      cand_dict["votes"][current_cand] = cand_dict["votes"][current_cand] + 1
+
+# Prepare winning candidate summary for export to the text file
+# Write the total vote count to the summary
+summary = f"""Election Results\n\
+-------------------------\n\
+Total Votes: {cand_dict["total"]}\n\
+-------------------------\n"""
+
+# Loop through the candidates to determine vote percentages and identify the winner
+for i in cand_dict["votes"].keys():
+  cand_dict["percent"][i] = f"{'{:.3%}'.format(cand_dict['votes'][i] / cand_dict['total'])}"
+
+  # Update the winning candidate if this one has more votes
+  if cand_dict["percent"][i] > win_calc:
+    win_calc = cand_dict["percent"][i]
+    winner = i
+
+  # Save each candidate's vote count and percentage
+  summary = summary + (f"{i}: {cand_dict["percent"][i]} ({cand_dict['votes'][i]})\n")
+
+# Save the winning candidate to the summary
+summary = summary + (f"-------------------------\nWinner: {winner}\n-------------------------")
+
+# Print the summary (to terminal)
+print(summary)
 
 # Open a text file to save the output
 with open(file_to_output, "w") as txt_file:
-
-    # Print the total vote count (to terminal)
-  print(
-f"\n\
-Election Results\n\
--------------------------\n\
-Total Votes: {total_votes}"
-  )
-
-  print(f"Dictionary: {cand_dict}")
-  print(f"List: {cand_list}")
-    # Write the total vote count to the text file
-
-    # Loop through the candidates to determine vote percentages and identify the winner
-  for i in list(cand_dict.keys()):
-     print(f"Look here: {cand_dict}")
-     print(f"Keys: {cand_dict[i]}")
-     percent_votes = cand_dict[i] / total_votes
-     #cand_dict[f"Percent{percent_votes}"] = percent_votes
-     perc_dict = {"Total": cand_dict[i], "Percent": percent_votes}
-     print(perc_dict)
-        # Get the vote count and calculate the percentage
-
-
-        # Update the winning candidate if this one has more votes
-
-
-        # Print and save each candidate's vote count and percentage
-
-
-    # Generate and print the winning candidate summary
-
-
-    # Save the winning candidate summary to the text file
+  txt_file.write(summary)
